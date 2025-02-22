0e618e521d6bb54b82cb9878e1eb935d0abeeac8
tanmay <kuamrchaurasiatanmay@gmail.com>
2025-02-23 01:21:36 +0530

    kafka comsumer written testing with mocha remaning

---
+++ apps/backend-admin/kafka/Consumer.ts
@@ -0,0 +1,37 @@
+import { Kafka } from "kafkajs";
+import jobModel from "../model/job.model";
+
+const kafka = new Kafka({
+    clientId: "admin-service",
+    brokers: ["localhost:9092"] 
+});
+
+const consumer = kafka.consumer({ groupId: "job-posting-group" });
+
+export const jobsconsumer = async () => {
+    await consumer.connect();
+    
+    await consumer.subscribe({
+        topic: "job-postings", 
+        fromBeginning: true
+    });
+
+    await consumer.run({
+        eachMessage: async ({ topic, partition, message }) => {
+            if (message.value) {
+                const data = JSON.parse(message.value.toString()); 
+                
+                console.log(`Received job posting:`, data);
+
+                try {
+                    await jobModel.create(data);
+                    console.log("Job saved to DB successfully");
+                } catch (err) {
+                    console.error("Error saving job to DB:", err);
+                }
+            } else {
+                console.warn("Received empty message");
+            }
+        }
+    });
+};
